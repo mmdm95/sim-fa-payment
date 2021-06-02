77a9267522650caebbc4774184ee2b24370272ab
MMDM <mmdehghan95@gmail.com>
2021-06-03 01:07:45 +0430

    make method chaining available

--- src/Providers/Sadad/SadadRequestProvider.php
+++ src/Providers/Sadad/SadadRequestProvider.php
@@ -16,50 +16,62 @@ class SadadRequestProvider extends AbstractParameterProvider
 
     /**
      * @param $amount
+     * @return static
      */
     public function setAmount($amount)
     {
         $this->parameters['Amount'] = $amount;
+        return $this;
     }
 
     /**
      * @param $returnUrl
+     * @return static
      */
     public function setReturnUrl($returnUrl)
     {
         $this->parameters['ReturnUrl'] = $returnUrl;
+        return $this;
     }
 
     /**
      * @param $localDateTime
+     * @return static
      */
     public function setLocalDateTime($localDateTime)
     {
         $this->parameters['LocalDateTime'] = $localDateTime;
+        return $this;
     }
 
     /**
      * @param $orderId
+     * @return static
      */
     public function setOrderId($orderId)
     {
         $this->parameters['OrderId'] = $orderId;
+        return $this;
     }
 
     /**
      * @param string $additionalData
+     * @return static
      */
     public function setAdditionalData(string $additionalData)
     {
         $this->parameters['AdditionalData'] = $additionalData;
+        return $this;
     }
 
     /**
      * @param $userId
+     * @return static
      */
     public function setUserId($userId)
     {
         $this->parameters['UserId'] = $userId;
+        return $this;
     }
 
     /**
